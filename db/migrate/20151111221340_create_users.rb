18989bcd529578a06a3477b7c6671f333cc9b25c
Sheri Zada <sheri.h.zada@gmail.com>
2015-11-15 15:37:30 -0800

    first commit

---
+++ db/migrate/20151111221340_create_users.rb
@@ -0,0 +1,21 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+
+    create_table :shortened_urls do |t|
+      t.string :short_url
+      t.string :long_url
+      t.integer :submitter_id
+
+      t.timestamps
+    end
+
+    add_index :shortened_urls, :submitter_id
+    add_index :shortened_urls, :short_url, unique: true
+  end
+end
